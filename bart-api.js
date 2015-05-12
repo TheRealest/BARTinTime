4b0b839363d407c19d75b9a292b9a873c194981f
TheRealest <real@rrprovencher.com>
2015-05-12 01:23:06 -0700

    added station XML processor code

--- bart-api.js
+++ bart-api.js
@@ -1,11 +1,34 @@
 var bartApi = {
   apiKey: 'BART_API_KEY',
-  url: {
-    stations: function() {
-       return 'http://api.bart.gov/api/stn.aspx?cmd=stns&key=' + bartApi.apiKey;
+  get: {
+    root: 'http://api.bart.gov/api/',
+    stations: {
+      link: function() { return bartApi.get.root + 'stn.aspx?'; },
+      data: function() { return { cmd: 'stns', key: bartApi.apiKey }; }
     },
-    time: function(station, direction) {
-      return ['http://api.bart.gov/api/etd.aspx?cmd=etd&orig=',station,'&dir=',direction,'&key=',bartApi.apiKey].join('');
+    times: {
+      link: function() { return bartApi.get.root + 'etd.aspx?'; },
+      data: function(station,dir) { return { cmd: 'etd', orig: station, dir: dir, key: bartApi.apiKey }; }
     }
   }
 }
+
+var getStationList = function(cb) {
+  var endpoint = bartApi.get.stations;
+  $.get(endpoint.link(),endpoint.data()).done(cb);
+};
+
+var processStationList = function(rawXml) {
+  var $xml = $(rawXml);
+  var stations = $xml.find('station');
+  var properties = ['name','abbr'];
+  stations = stations.map(function(i,elem) {
+    var obj = {};
+    properties.forEach(function(prop) {
+      obj[prop] = $(elem).find(prop).text();
+    });
+    return obj;
+  }).get();
+  return stations;
+};
+
